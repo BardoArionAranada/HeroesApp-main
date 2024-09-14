ee4fe2da94d408ccb5dca0ab63e3f36cbaeddc8d
arion aranda <arioncronos15s@gmail.com>
2024-09-14 00:39:04 -0600

    agregar la clase PublisherActivity en mi carpeta de Activity

---
+++ app/src/main/java/com/example/heroesapp_main/activities/PublisherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.heroesapp_main.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.heroesapp_main.R
+
+class PublisherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publisher)
+    }
+}
